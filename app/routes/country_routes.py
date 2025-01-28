e5d7379a080adf79265a374b9dbb8966aee4d009
Jenny Massari <jennymartins13@gmail.com>
2025-01-27 20:57:35 -0600

    First final version of endpoints without image feature.

--- app/routes/country_routes.py
+++ app/routes/country_routes.py
@@ -2,28 +2,49 @@ from flask import Blueprint, abort, make_response, request, Response
 from app.models.country import Country
 from app.models.experience import Experience
 from app.db import db
-from app.routes.route_utilities import validate_model
+from app.routes.route_utilities import validate_model, create_model, get_models_with_filters
 import os
 
-bp = Blueprint("country_bp", __name__, url_prefix="/coutry")
-
-# C- Not creating a country, but the user should be able to post if it was borned, visited or want to visit the country.
-# R - Not readind all countries, but getting all the users post when page is loaded
-# U - Update the user info about the country
-# D - Unselect all options?
-
-@bp.post("/<country_id>")
-def select_country_option(country_id):
-    pass
+bp = Blueprint("country_bp", __name__, url_prefix="/country")
 
 @bp.get("")
 def gel_all_options():
-    pass
+    return get_models_with_filters(Country, request.args)
 
 @bp.patch("/<country_id>")
-def update_an_option(country_id):
-    pass
+def select_country_option(country_id):
+    country = validate_model(Country, country_id)
 
-@bp.delete("/<country_id>")
-def delete_an_option(country_id):
-    pass
+    data = request.get_json()
+    borned = data.get('borned', False)
+    visited = data.get('visited', False)
+    want_to_visit = data.get('want_to_visit', False)
+
+    country.visited = visited
+    country.borned = borned
+    country.want_to_visit = want_to_visit
+
+    db.session.commit()
+
+    response_body = {
+        "message": "Successfully updated the country options",
+        "country": country.to_dict() 
+    }
+    return make_response(response_body, 200)
+
+
+@bp.put("/<country_id>/unselect")
+def unselect_country_option(country_id):
+    country = validate_model(Country, country_id)
+
+    country.visited = False
+    country.borned = False
+    country.want_to_visit = False
+
+    db.session.commit()
+
+    response_body = {
+        "message": f"The option for country {country_id} has been unselected.",
+        "country": country.to_dict()  
+    }
+    return make_response(response_body, 200)
--- app/routes/experience_routes.py
+++ app/routes/experience_routes.py
@@ -1,25 +1,22 @@
 from flask import Blueprint, abort, make_response, request
 from app.models.experience import Experience
+from app.models.country import Country
 from app.db import db
 from app.routes.route_utilities import validate_model
 import os
 
 bp = Blueprint("experience_bp", __name__, url_prefix="/experiences")
 
-# C - user can post an experience adding a title, a description and an image
-# R - user can read all the experiences for the country selected
-# U - user can update the title, description and image
-# D - user can delete the whole experience or delete only the picture(optional)
-
 @bp.get("")
 def get_all_experiences():
     query = db.select(Experience)
     
     query= query.order_by(Experience.id)
-    cards = db.session.scalars(query)
+    experiences = db.session.scalars(query)
     
     response_body = [experience.to_dict() for experience in experiences]
     return response_body
+
 
 @bp.get("/<experience_id>")
 def get_a_experience(experience_id):
@@ -28,24 +25,37 @@ def get_a_experience(experience_id):
     db.session.add(experience)
     db.session.commit()
 
-@bp.post("")
-def create_experience():
-    request_body = request.get_json()
-    title = request_body["title"]
-    description = request_body["description"]
-    #image = if in the backend/ optional
-
-    new_experience = Experience(title=title, description=description)
-    db.session.add(new_experience)
-    db.session.commit()
-
-    response_body = new_experience.to_dict()
-
-    return response_body,201
+    return make_response(experience.to_dict(), 200)
 
 @bp.delete("/<experience_id>")
 def delete_a_experience(experience_id):
     experience = validate_model(Experience, experience_id)
     
+    db.session.delete(experience)
+    db.session.commit()
+
     response_body = {"details": f'Experience {experience_id} "{experience.title}" successfully deleted.'}
     return make_response(response_body, 200)
+
+@bp.post("")
+def create_experience_to_a_country():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    country_id = request_body["country_id"]
+
+    #image = if in the backend/ optional
+
+    country = validate_model(Country, country_id)
+
+    new_experience = Experience(title=title, description=description, country_id=country_id)
+    db.session.add(new_experience)
+    db.session.commit()
+
+
+    response_body = new_experience.to_dict()
+
+    return make_response(response_body, 201)
+
+
+
