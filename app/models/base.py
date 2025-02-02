ec5f615b7ee3ef57ba6d23ee02cb54738a66a341
Jenny Massari <jennymartins13@gmail.com>
2025-02-01 18:19:08 -0800

    Fix DeclarativeBase import for SQLAlchemy 2.x

--- app/models/base.py
+++ app/models/base.py
@@ -1,4 +1,7 @@
-from sqlalchemy.orm import DeclarativeBase
+# from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-    pass
+# class Base(DeclarativeBase):
+#     pass
+
+from sqlalchemy.orm import declarative_base
+Base = declarative_base()
