3e0da56da869ff8a59a370fbc8ce90fc7df5eae8
Jenny Massari <jennymartins13@gmail.com>
2025-02-01 18:54:20 -0800

    Remove mapped_columns to Column

--- app/models/country.py
+++ app/models/country.py
@@ -1,17 +1,31 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship 
+#from sqlalchemy.orm import Mapped, mapped_column, relationship 
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 from ..db import db
 from app.models.experience import Experience
 
 
 class Country(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str]
-    lat: Mapped[int]
-    long: Mapped[int]
-    visited: Mapped[bool]
-    borned: Mapped[bool]
-    want_to_visit: Mapped[bool]
-    experiences: Mapped[list["Experience"]] = relationship("Experience", back_populates="country")
+    # id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    # name: Mapped[str]
+    # lat: Mapped[int]
+    # long: Mapped[int]
+    # visited: Mapped[bool]
+    # borned: Mapped[bool]
+    # want_to_visit: Mapped[bool]
+    # experiences: Mapped[list["Experience"]] = relationship("Experience", back_populates="country")
+
+    __tablename__ = 'countries'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    lat = Column(Integer, nullable=False)
+    long = Column(Integer, nullable=False)
+    visited = Column(Boolean, nullable=False)
+    borned = Column(Boolean, nullable=False)
+    want_to_visit = Column(Boolean, nullable=False)
+    experiences = relationship("Experience", back_populates="country")
+
 
     def to_dict(self):
         return {
--- app/models/experience.py
+++ app/models/experience.py
@@ -1,18 +1,31 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+#from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from ..db import db
 from sqlalchemy import ForeignKey
 from typing import Optional
 
 
 class Experience(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str]
-    description: Mapped[str]
-    image = db.Column(db.String(255), nullable=True)
+    # id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    # title: Mapped[str]
+    # description: Mapped[str]
+    # image = db.Column(db.String(255), nullable=True)
 
 
-    country_id: Mapped[Optional[int]] = mapped_column(ForeignKey("country.id")) 
-    country: Mapped[Optional["Country"]] = relationship(back_populates="experiences")
+    # country_id: Mapped[Optional[int]] = mapped_column(ForeignKey("country.id")) 
+    # country: Mapped[Optional["Country"]] = relationship(back_populates="experiences")
+
+    __tablename__ = 'experiences'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    image = Column(String(255), nullable=True)
+
+    country_id = Column(Integer, ForeignKey("country.id"), nullable=True)
+    country = relationship("Country", back_populates="experiences")
+
 
     def to_dict(self):
         return {
